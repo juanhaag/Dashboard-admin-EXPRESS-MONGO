dbeee8505afe5768019bfcaf513af4196def3a1e
unknown <apetroxtrm@gmail.com>
2023-02-24 23:00:34 -0300

    add cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const cors = require('cors');
 
 const app = express();
-
+app.use(cors()); 
 // capturar body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
